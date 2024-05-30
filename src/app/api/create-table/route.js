14e90f02b95f6732772886b28c88adedcadb9af7
Nathan GAUD <nathan.gaud@gmail.com>
2024-05-31 01:41:03 +0200

    Delete endpoint add-user

--- src/app/api/create-table/route.js
+++ src/app/api/create-table/route.js
@@ -12,12 +12,7 @@ export async function GET(request) {
                     slot integer,
                     price float8
                 )
-                CREATE TABLE IF NOT EXISTS Users (
-                    wallet_address char(42),
-                    protected_data varchar(255),
-                    platform varchar(255),
-                );
-        `;
+            `;
             return NextResponse.json({ result }, { status: 200 });
     } catch (error) {
         return NextResponse.json({ error }, { status: 500 });
